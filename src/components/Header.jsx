82add53316e06d82b1d281bf4574c3084df692e6
Yisrael Grimes <yisraelgrimes@gmail.com>
2020-02-06 17:51:00 -0600

    Added site logo as an accessible inline svg

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,37 @@
 import React from "react"
-import { Link } from "gatsby"
-import { Nav } from "./Nav"
+import { useStaticQuery, graphql, Link } from "gatsby"
 
+import { Nav } from "./Nav"
+import { LogoInlineSvg } from "./LogoInlineSvg"
+// import logo from "../images/logo.svg"
+
+
+const _GET_TITLE = graphql`
+	query {
+		site {
+			siteMetadata {
+				title
+			}
+		}
+	}
+`
 
 const Header = () => {
+	const { site } = useStaticQuery(_GET_TITLE)
+
 	return (
 		<header>
 			{/* Site Logo */}
 			<Link to="/">
-				Site Logo Image
-				{/* {data.site.siteMetadata.title} */}
-				{/* Brand Logo -- see file for info about props */}
-				{/* <Logo /> */}
+				{site.siteMetadata.title}
+				{/* Div around logo is temporary */}
+				<div style={{height: `100px`, width: `100px`, color:`#000`}}>
+					<LogoInlineSvg
+						fillColor="orange"
+						title="SpaceCat"
+						desc="This is a cat wit an astronaut helmet"
+					/>
+				</div>
 			</Link>
 
 			<Nav />
