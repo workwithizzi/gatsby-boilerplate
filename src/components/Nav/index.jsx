57e2143115b63bf93225b17bb0a9a97745950768
Yisrael Grimes <yisraelgrimes@gmail.com>
2020-02-10 15:10:55 -0600

    Cleaned up Nav component and separated MainMenu component

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,134 +1,60 @@
 import React, { Component } from "react"
-import { Link, graphql, useStaticQuery } from "gatsby"
 
 import { OpenIcon } from "../svgs/OpenIcon"
 import { CloseIcon } from "../svgs/CloseIcon"
+import { MainMenu } from "./MainMenu"
 
 // import "./nav.sass"
 import styles from "./nav.module.sass"
 
 
-// const _QUERY_MENU = graphql`
-// 	query {
-// 		site {
-// 			siteMetadata {
-// 				title
-// 				menuLinks {
-// 					name
-// 					path
-// 				}
-// 			}
-// 		}
-// 	}
-// `
-
-const _QUERY_MENU = graphql`
-	query {
-		site {
-			siteMetadata {
-				title
-				menuLinks {
-					name
-					path
-					childMenuLinks {
-						name
-						path
-					}
-				}
-			}
-		}
-	}
-`
-
-
-const MainMenu = () => {
-	const data = useStaticQuery(_QUERY_MENU)
-	const menuLinks = data.site.siteMetadata.menuLinks
-	return (
-		<nav>
-			<ul>
-				{menuLinks.map(link => (
-					<li key={link.name}>
-						<Link to={link.path}>
-							{link.name}
-						</Link>
-						{link.childMenuLinks && (
-							link.childMenuLinks.map(link =>(
-								<li key={link.name}>
-									<Link to={link.path}>
-										{link.name}
-									</Link>
-								</li>
-							))
-
-						)}
-					</li>
-				))}
-
-			</ul>
-		</nav>
-	)
-}
-
-
 // Using react to conditionally render which menu button to show
 // Using css to hide/show the menu depending on state so that the
 // menu will always be accessible for screen-readers and SEO
-
 class Nav extends Component {
-	constructor(props) {
-		super(props)
-
-		// When the button is clicked, change the value
-		this.state = { addClass: false }
+	state = {
+		showMenu: false,
 	}
-
-	// this is the function that changes the state onClick event
-	_toggleClassChange = e => {
-		this.setState({ addClass: !this.state.addClass })
-	}
-
 	render() {
-		// These classes are added to <nav> and <button> on first render
-		const navClasses = [styles.nav]
-		// When button is clicked:
-		if (this.state.addClass) {
-			navClasses.push(styles.navIsActive)
-		}
-		return (
-			<nav id="js-nav" role="navigation" className={navClasses.join(` `)}>
-				{/* Responsive Nav Buttons */}
-				<div className={styles.toggleButtonContainer}>
-					<button
-						id="js-toggle-menu"
-						type="button"
-						aria-expanded="false"
-						aria-controls="main-menu"
-						className={styles.toggleButton}
-						// onClick, toggle state and toggle 'is-active' classes
-						onClick={this._toggleClassChange}
-					>
-						{!this.state.addClass ? (
-							<div>
-								<span>Open Menu</span>
-								<OpenIcon />
-							</div>
-						) : (
-							<div>
-								<span>Close Menu</span>
-								<CloseIcon />
-							</div>
-						)}
-					</button>
-				</div>
 
-				{/* Main Menu */}
-				{/* Use css to hide/show menu for accessibility and SEO */}
-				<MainMenu />
+		const ButtonContent = this.state.showMenu ? (
+			<>
+				<span className="sr-only">Close Menu</span>
+				<CloseIcon />
+			</>
+		) : (
+			<>
+				<span className="sr-only">Open Menu</span>
+				<OpenIcon />
+			</>
+		)
+
+		const _menuClass = this.state.showMenu ? (
+			styles.mainMenu + styles.isActive
+		) : (
+			styles.mainMenu
+		)
+
+		return (
+			<nav className={styles.nav} role="navigation">
+				{/* Responsive Button */}
+				<button
+					type="button"
+					aria-expanded="false"
+					aria-controls="mainMenu"
+					className={styles.buttonToggle}
+					onClick={() => {
+						this.setState({ showMenu: !this.state.showMenu })
+					}}
+				>
+					{ButtonContent}
+				</button>
+
+				<MainMenu id="mainMenu" className={_menuClass} />
 			</nav>
 		)
 	}
 }
 
 
-export { Nav, MainMenu }
+export { Nav }
